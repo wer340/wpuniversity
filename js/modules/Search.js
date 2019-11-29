dc5cecde405fe045260a05ca3499b36037efb4e3
hossein.s <hsee1372@gmail.com>
2019-11-29 22:59:26 +0330

    javafullsearchboxLiveAndspinnerhandle

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 class Search {
 
     constructor(){
+        this.preSetTime;
+        this.spinnerVisible=false ;
         this.resultDiv=$('#search-overlay__results');
         this.openButton=$('.js-search-trigger');
         this.overlay=$('.search-overlay');
@@ -15,16 +17,35 @@ class Search {
         this.openButton.on('click',this.openOverlay.bind(this));
         this.closeButton.on('click',this.closeOverlay.bind(this));
         $(document).on("keydown",this.keyOverlay.bind(this));//'keyup' without lag and press leave a time run
-        this.SetTime.on('keydown',this.timeLogic.bind(this));
+        this.SetTime.on('keyup',this.timeLogic.bind(this));
     }
+
+
     timeLogic(){
-        clearTimeout( this.timeVar);
-        this.timeVar=setTimeout(function () {
-            console.log('this type in search box');
-        },4000)//arg 1-func arg2 delay
+
+        if (this.SetTime.val()!= this.preSetTime){//identify difference add remove char with move cursor
+            clearTimeout( this.timeVar);
+            if(this.SetTime.val()){
+                if(!this.spinnerVisible){//no run more this line
+                    this.resultDiv.html('<div class="spinner-loader"></div>');
+                    this.spinnerVisible=true;
+                }
+                this.timeVar=setTimeout(this.getResults.bind(this),2000)//arg 1-func arg2 delay
+
+            }else{
+                this.resultDiv.html('');
+                this.spinnerVisible=false;
+            }
+
+        }
+        this.preSetTime=this.SetTime.val();
+
     }
+
+
     getResults(){
         this.resultDiv.html("welcome to type succesfull");
+        this.spinnerVisible=false;
     }
     keyOverlay(e){
     // console.log(e.keyCode);
